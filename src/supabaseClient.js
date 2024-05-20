2469fe4409727b38d2ba5e7d173b7026d96f51b9
ksapaev <39306914+ksapaev@users.noreply.github.com>
2024-05-20 13:52:31 -0400

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://ejccknvgsubhhvnvjqwo.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqY2NrbnZnc3ViaGh2bnZqcXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ0MTI0MjYsImV4cCI6MjAyOTk4ODQyNn0.BxPWzY3j9IPG8lpH2Dk6QT79Z3gpO6dviTy6Z4Ml4YM';
+const supabaseKey = 'key';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
